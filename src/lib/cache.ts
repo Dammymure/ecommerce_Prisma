233b616cad7d0dc4cca703ebbc3ecda483ae3ab9
Dammymure <dammymure1@gmail.com>
2024-12-13 18:04:50 +0100

    vercel updates

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -14,16 +14,23 @@
 import { unstable_cache as nextCache } from "next/cache";
 import { cache as reactCache } from "react";
 
-type Callback = (...args: any[]) => Promise<any>;
+type Callback<Args extends unknown[] = unknown[], Return = unknown> = (...args: Args) => Promise<Return>;
 
 export function cache<T extends Callback>(
     cb: T,
     keyParts: string[],
     options: { revalidate?: number | false; tags?: string[] } = {}
 ) {
-    const cachedCallback = reactCache(cb); // Cache the callback function using react's cache
-    return nextCache(async (...args: Parameters<T>): Promise<ReturnType<T>> => {
-        const cacheKey = keyParts.join(":");
-        return cachedCallback(...args);
-    }, keyParts, options);
+    const cachedCallback = reactCache(cb); // Cache the callback function using React's cache
+
+    return nextCache(
+        async (...args: Parameters<T>): Promise<ReturnType<T>> => {
+            const _cacheKey = keyParts.join(":"); // For potential debugging or future use
+            return (await cachedCallback(...args)) as ReturnType<T>; // Explicitly cast to ReturnType<T>
+        },
+        keyParts,
+        options
+    );
 }
+
+
