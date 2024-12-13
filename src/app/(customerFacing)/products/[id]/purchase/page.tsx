2861010367395b695fd2941ff8c8e7f774e5382f
Dammymure <dammymure1@gmail.com>
2024-12-13 18:36:36 +0100

    vercel updates

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,63 +1,30 @@
-// import db from "@/db/db"
-// import { notFound } from "next/navigation"
-// import Stripe from "stripe"
-// import { CheckoutForm } from "./_components/CheckoutForm"
+import db from "@/db/db"
+import { notFound } from "next/navigation"
+import Stripe from "stripe"
+import { CheckoutForm } from "./_components/CheckoutForm"
 
-// // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
-// export default async function PurchasePage({
-//     params: { id } }: {
-//      params: { id: string } 
-//     }){
-//     const product = await db.product.findUnique({ where: { id }})
-//     if (product == null) return notFound()
-
-//     const paymentIntent = await stripe.paymentIntents.create({
-//         amount: product.priceInCents,
-//         currency: "USD",
-//         metadata: { productId: product.id }
-//     })
-
-//     if (paymentIntent.client_secret == null){
-//         throw Error("Stripe failed to create payment intent")
-//     }
-
-//     return <CheckoutForm 
-//     product={product}
-//     clientSecret={paymentIntent.client_secret}
-//     />
-// }
-
-import db from "@/db/db";
-import { notFound } from "next/navigation";
-import Stripe from "stripe";
-import { CheckoutForm } from "./_components/CheckoutForm";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-
-interface PurchasePageProps {
-    params: { id: string };
-}
-
-export default async function PurchasePage({ params }: PurchasePageProps) {
-    const product = await db.product.findUnique({ where: { id: params.id } });
-    if (product == null) return notFound();
+export default async function PurchasePage({
+    params: { id } }: {
+     params: { id: string } 
+    }){
+    const product = await db.product.findUnique({ where: { id }})
+    if (product == null) return notFound()
 
     const paymentIntent = await stripe.paymentIntents.create({
         amount: product.priceInCents,
         currency: "USD",
-        metadata: { productId: product.id },
-    });
+        metadata: { productId: product.id }
+    })
 
-    if (!paymentIntent.client_secret) {
-        throw new Error("Stripe failed to create payment intent");
+    if (paymentIntent.client_secret == null){
+        throw Error("Stripe failed to create payment intent")
     }
 
-    return (
-        <CheckoutForm 
-            product={product} 
-            clientSecret={paymentIntent.client_secret} 
-        />
-    );
+    return <CheckoutForm 
+    product={product}
+    clientSecret={paymentIntent.client_secret}
+    />
 }
